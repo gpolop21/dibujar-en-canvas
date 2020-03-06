90240db9364e716a2324e05ef2437af63a2801e5
greys <gpolop21@gmail.com>
2020-03-05 19:49:42 -0500

    aprender a dibujar en canvas

---
+++ dibujo.js
@@ -0,0 +1,18 @@
+var d = document.getElementById("dibujito");
+var lienzo = d.getContext("2d");
+console.log(lienzo);
+
+dibujarLinea("pink", 10, 300, 220, 10);
+dibujarLinea("black", 100, 200, 120, 110);
+
+function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
+{
+  lienzo.beginPath();
+  lienzo.strokeStyle = color ;
+  lienzo.moveTo(xinicial, yinicial);
+  lienzo.lineTo(xfinal, yfinal);
+  lienzo.stroke();
+  lienzo.closePath();
+
+
+}
