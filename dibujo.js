60f049115dadcc76b4eeec7f87bd815ff20fbae0
greys <gpolop21@gmail.com>
2020-03-09 21:34:48 -0500

    esperimentar  con canvas mañana

--- dibujo.js
+++ dibujo.js
@@ -1,35 +1,37 @@
+var texto = document.getElementById("texto_lineas");
+var boton = document.getElementById("botoncito");
+boton.addEventListener("click", dibujoPorClick );
+
 var d = document.getElementById("dibujito");
+var ancho = d.width;
 var lienzo = d.getContext("2d");
-var linea = 30;
-var l = 0;
-var yi, xf;
-var colorcito =  "#FAA";
-var colorcita = "blue";
-
-for(l=0; l < linea; l++)
-{
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  dibujarLinea(colorcito, 300, yi, xf, 0);
-  dibujarLinea(colorcita, 0, yi, xf, 300);
-  console.log("linea" + l);
-
-
-}
-
-
-
-
-
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
 {
   lienzo.beginPath();
-  lienzo.strokeStyle = color ;
+  lienzo.strokeStyle = color;
   lienzo.moveTo(xinicial, yinicial);
   lienzo.lineTo(xfinal, yfinal);
   lienzo.stroke();
   lienzo.closePath();
+}
 
+function dibujoPorClick()
+{
+  var lineas = parseInt(texto.value);
+  var l = 0;
+  var yi, xf;
+  var colorcito = "#FAA";
+  var espacio = ancho / lineas;
 
+  for(l = 0; l < lineas; l++)
+  {
+    yi = espacio * l;
+    xf = espacio * (l + 1);
+    dibujarLinea(colorcito, 0, yi, xf, 300);
+    console.log("Linea " + l);
+  }
+
+  dibujarLinea(colorcito, 1,1,1,299);
+  dibujarLinea(colorcito, 1,299,299,299);
 }
